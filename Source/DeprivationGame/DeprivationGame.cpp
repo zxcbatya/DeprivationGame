d199533938d992177469fc3bdabebf53dceed3e7
zxcbatya <alieksandr.gunari.06@mail.ru>
2025-04-12 20:39:28 +0500

    First commit

---
+++ Source/DeprivationGame/DeprivationGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeprivationGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeprivationGame, "DeprivationGame" );

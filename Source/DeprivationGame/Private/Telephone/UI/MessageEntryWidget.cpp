61fdd23f2bcbdfcb525aea513bf18ae9361e1d9b
zxcbatya <alieksandr.gunari.06@mail.ru>
2025-11-13 00:24:15 +0500

    Creating base telephone System

---
+++ Source/DeprivationGame/Private/Telephone/UI/MessageEntryWidget.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Telephone/UI/MessageEntryWidget.h"
+
+void UMessageEntryWidget::SetMessage(const FMessageData& Message)
+{
+	// Implementation will depend on your UI elements
+	// For example:
+	// if (MessageTextBlock)
+	// {
+	//     MessageTextBlock->SetText(FText::FromString(Message.Content));
+	// }
+}
